39582d0ac19d43b359b488adeddeffa4c30dfe9b
Kwame Porter Robinson <kporterrobinson@gmail.com>
2021-07-29 12:03:41 -0400

    (feat) Template for fast-api + tf @kushalvala, modified to run with uvi-gun-ml-docker image without overriding host, port or conflicting with its other defaults

---
+++ online-learner/app/main.py
@@ -0,0 +1,36 @@
+import uvicorn
+from typing import List
+from pydantic import BaseModel
+from fastapi import FastAPI
+import tensorflow as tf
+app = FastAPI()
+
+model = tf.keras.models.load_model('../model/tf_keras_imdb')
+
+
+class Reviews(BaseModel):
+    review: str
+
+
+@app.get('/')
+def index():
+    return {'message': 'This is IMDb Reviews Classification API! Hello'}
+
+# this is new
+
+@app.post('/predict')
+def predict_review(data: Reviews):
+    """ FastAPI 
+
+    Args:
+        data (Reviews): json file 
+
+    Returns:
+        prediction: probability of review being positive
+    """
+    data = data.dict()
+    review = data['review']
+    prediction = model.predict([review])
+    return {
+        'prediction': prediction.tolist()[0][0]
+    }
